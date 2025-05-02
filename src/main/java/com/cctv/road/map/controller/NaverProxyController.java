84678c6976cb1b56d593996e016068e6d1af66ba
Hoooouuuuu <whdgh91@gmail.com>
2025-05-02 19:27:57 +0900

    로고 교체

--- src/main/java/com/cctv/road/map/controller/NaverProxyController.java
+++ src/main/java/com/cctv/road/map/controller/NaverProxyController.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
